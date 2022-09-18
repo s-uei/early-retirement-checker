bc0a5166ae79ab8c97b52b5594928cd1fbb75a98
s-uei <uei.shunsuke@kagawa-u.ac.jp>
2022-09-18 12:20:20 +0900

    重なり修正

--- components/Appbar.tsx
+++ components/Appbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Appbar(props: { children?: React.ReactNode }) {
   return (
-    <div className="fixed top-0 flex w-full bg-black text-white items-center">
+    <div className="fixed top-0 flex w-full bg-black text-white items-center z-50">
       {props.children}
     </div>
   );
